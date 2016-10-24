f911daf3aa6271503e28a75555d5bad1e75262e5
Abraham McIlvaine <abemac@udel.edu>
2016-10-24 13:16:42 -0400

    finished johnson alg

--- elem_cycle/shortest_path/main.cpp
+++ elem_cycle/shortest_path/main.cpp
@@ -4,38 +4,130 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <list>
+#include <algorithm>
+
+std::vector<std::list<int> > A,B;
+std::vector<bool> blocked;
+int s;
+
+std::list<int> stack;
 
 
-std::vector<std::vector<unsigned long long> > src_file;
+void init(const std::string& filePath);
+void UNBLOCK(int u);
+bool CIRCUIT(int v);
+void OUTPUT();
 
-void read_in_file(std::string path);
 
 int main(){
-  
+  init("test_graphs/test3.csv");
+
+  s=0;
+
+  while (s < A.size()){
+    CIRCUIT(s);
+    A[s].clear();
+    for (int i =0;i<A.size();++i){
+      A[i].remove(s);
+    }
+    for (int i =0;i<A.size();++i){
+      blocked[i]=false;
+      B[i].clear();
+    }
+    ++s;
+  }
+
+
 
 
 
 }
 
+bool CIRCUIT(int v){
+  bool f = false;
+  stack.push_back(v);
+  blocked[v]=true;
 
-void read_in_file(const std::string& filePath){
+  for(auto itr = A[v].begin();itr!=A[v].end();++itr){
+    int w = *itr;
+    if(w==s){
+      OUTPUT();
+      f=true;
+    }else if (!blocked[w]){
+      if(CIRCUIT(w)){
+        f=true;
+      }
+    }
+  }
+  if(f){
+    UNBLOCK(v);
+  }
+  else{
+    for(auto itr = A[v].begin();itr!=A[v].end();++itr){
+      int w = *itr;
+      if(std::find(B[w].begin(),B[w].end(),v)== B[w].end()){
+        B[w].push_back(v);
+      }
+    }
+  }
+  stack.pop_back();
+  return f;
+
+
+
+}
+
+void UNBLOCK(int u){
+  blocked[u]=false;
+  for(auto itr = B[u].begin();itr!=B[u].end();){
+    int w = *itr;
+    itr=B[u].erase(itr);
+    if(blocked[w]){
+      UNBLOCK(w);
+    }
+  }
+}
+
+
+void OUTPUT(){
+    int w=0;
+    for (auto iw = stack.begin(); iw != stack.end(); ++iw)
+        {
+           w++;
+        }
+    if(w%2==1 && w>2)//yuan changed
+      {
+    for (auto iw = stack.begin(); iw != stack.end(); ++iw)
+        std::cout << *iw << "->";
+
+    std::cout <<s<< std::endl;
+  }
+    return;
+}
+
+void init(const std::string& filePath){
   std::ifstream FILE(filePath);
   std::string str;
 
   while(std::getline(FILE,str)){
-    std::vector<unsigned long long> tmp;
-    unsigned long long i=0;
-    unsigned long long lasti=0;
+    std::list<int> tmp;
+    int i=0;
+    int lasti=0;
     for(;i<str.size();i++){
       if(str[i]==',' || i==str.size()-1){
-        unsigned long long val=std::stoull(str.substr(lasti,i+1-lasti),nullptr,10);
+        int val=std::stoi(str.substr(lasti,i+1-lasti),nullptr,10);
         tmp.push_back(val);
         lasti=i+1;
       }
     }
-    src_file.push_back(tmp);
+    A.push_back(tmp);
    }
-
+   B.resize(A.size());
+   blocked.resize(A.size());
+   for(int i = 0;i<A.size();i++){
+     blocked[i]=false;
+   }
    FILE.close();
 
 }
