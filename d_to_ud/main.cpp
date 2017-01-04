6bd2ea9645bb7faaeaa7f13223b5676da0c59430
Abraham McIlvaine <abemac@udel.edu>
2017-01-04 16:27:12 -0500

    finished d_to_ud script

--- d_to_ud/main.cpp
+++ d_to_ud/main.cpp
@@ -8,8 +8,10 @@
 #include <set>
 
 void readFile(std::string& filePath);
-void rm_dups(std::list<int>& tmp,int current_state);
-std::vector<std::list<int> > adjList;
+void writeFile(std::string& filePath);
+void rm_current_state(std::set<int>& tmp,int current_state);
+void to_und();
+std::vector<std::set<int> > adjList;
 
 
 int main (int argc, char* argv[]){
@@ -27,7 +29,33 @@ int main (int argc, char* argv[]){
     }
     std::cout<<std::endl;
   }
+  to_und();
+  std::cout<<"undirected:"<<std::endl;
+  for(auto i : adjList){
+    for(auto j : i){
+      std::cout<<j<<" ";
+    }
+    std::cout<<std::endl;
+  }
+  filePath.pop_back();
+  filePath.pop_back();
+  filePath.pop_back();
+  filePath.pop_back();
+  filePath.append("_und.csv");
+  std::string pre="undirected_benchmarks";
+  filePath= pre + filePath.substr(10,filePath.size()-10);
+  writeFile(filePath);
+}
 
+
+void to_und(){
+  for(int i =0;i<adjList.size();i++){
+    for(int j : adjList[i]){
+      if(adjList[j].count(i)==0){
+        adjList[j].insert(i);
+      }
+    }
+  }
 }
 
 void readFile(std::string& filePath){
@@ -35,17 +63,17 @@ void readFile(std::string& filePath){
   std::string str;
   int count=0;
   while(std::getline(FILE,str)){
-    std::list<int> tmp;
+    std::set<int> tmp;
     int i=0;
     int lasti=0;
     for(;i<str.size();i++){
       if(str[i]==',' || i==str.size()-1){
         int val=std::stoi(str.substr(lasti,i+1-lasti),nullptr,10)-1;
-        tmp.push_back(val);
+        tmp.insert(val);
         lasti=i+1;
       }
     }
-    rm_dups(tmp,count);   //remove duplicates
+    rm_current_state(tmp,count);   //remove duplicates
     ++count;
     adjList.push_back(tmp);
    }
@@ -53,15 +81,26 @@ void readFile(std::string& filePath){
 
 
 }
-void rm_dups(std::list<int>& tmp,int current_state){
-  std::set<int> unique;
-  for(int i : tmp){
-    if(i!=current_state){
-      unique.insert(i);
+void rm_current_state(std::set<int>& tmp,int current_state){
+  tmp.erase(current_state);
+}
+
+void writeFile(std::string& filePath){
+  std::ofstream FILE(filePath);
+  for(auto set : adjList){
+    bool first=true;
+    for(int j: set){
+      if(first){
+        FILE << j;
+        first=false;
+      }
+      else{
+        FILE<<","<<j;
+      }
+
     }
+    FILE<<"\n";
   }
-  tmp.clear();
-  for(int i : unique){
-      tmp.push_back(i);
-  }
+
+  FILE.close();
 }
